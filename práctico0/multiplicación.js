0160c7cdbc749b0cec126ee04b5b2b3bfc1e66a3
Cecilia Tomé <ceciliainestome@gmail.com>
2023-05-23 12:55:49 -0300

    Primer commit de dos archivos

---
+++ práctico0/multiplicación.js
@@ -0,0 +1,6 @@
+var numero1 = 2
+var numero2 = 17
+var numero3 = 6
+var resultado = numero1*numero2*numero3
+console.log("El resultado de la multiplicación es: " + resultado)
+
---
+++ práctico0/resta.js
@@ -0,0 +1,4 @@
+var minuendo = 10
+var sustraendo = 2
+var resultado = minuendo - sustraendo
+console.log ("El resultado de la resta es: " + resultado)
