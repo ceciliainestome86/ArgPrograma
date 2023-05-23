2fa6be1456d84a742170fcc8210ffb3e8b7154df
Cecilia Tomé <ceciliainestome@gmail.com>
2023-05-23 12:56:28 -0300

    Agrego el archivo conversionGrados.js

---
+++ práctico0/conversionGrados.js
@@ -0,0 +1,7 @@
+var fahrenheit = 50
+var celsius = (fahrenheit - 32)*(5/9)
+console.log("La temperatura en Celsius es: " + celsius)
+
+var fahrenheit = 75
+var celsius = (fahrenheit - 32)*(5/9)
+console.log("La temperatura en Celsius es: " + celsius)
