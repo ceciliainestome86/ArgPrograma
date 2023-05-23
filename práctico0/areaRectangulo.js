130f0637c293e97472c9d3992c93e52da46ef33e
Cecilia Tomé <ceciliainestome@gmail.com>
2023-05-23 12:57:09 -0300

    Practico N0 finalizado

---
+++ práctico0/areaRectangulo.js
@@ -0,0 +1,8 @@
+var base = 5
+var altura = 3
+var area = base*altura
+console.log ("El área es: " + area)
+var base = 12.5
+var altura = 5.3
+var area = base*altura
+console.log ("El área es: " + area)
---
+++ práctico0/promedio3.js
@@ -0,0 +1,17 @@
+var numero1 = 5
+var numero2 = 7
+var numero3 = 9
+var promedio = (numero1 + numero2 + numero3) / 3
+console.log("El promedio es: " + promedio)
+
+var numero1 = -2
+var numero2 = -3
+var numero3 = -8
+var promedio = (numero1 + numero2 + numero3) / 3
+console.log("El promedio es: " + promedio)
+
+var numero1 = -1.2
+var numero2 = 5.3
+var numero3 = -3.3
+var promedio = (numero1 + numero2 + numero3) / 3
+console.log("El promedio es: " + promedio)
