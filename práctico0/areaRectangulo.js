d31bab4e6176c12ee2cec2d965d9a197922d2eb3
Cecilia Tomé <ceciliainestome@gmail.com>
2023-05-23 21:29:00 -0300

    Practico N0 finalizado

--- práctico0/areaRectangulo.js
+++ práctico0/areaRectangulo.js
@@ -1,8 +1,8 @@
-var base = 5
-var altura = 3
-var area = base*altura
-console.log ("El área es: " + area)
-var base = 12.5
-var altura = 5.3
-var area = base*altura
-console.log ("El área es: " + area)
+let base = 5
+let altura = 3
+let area = base*altura
+console.log ("El área del primer rectángulo es: " + area)
+base = 12.5
+altura = 5.3
+area = base*altura
+console.log ("El área del segundo rectángulo es: " + area)
--- práctico0/promedio3.js
+++ práctico0/promedio3.js
@@ -1,17 +1,17 @@
-var numero1 = 5
-var numero2 = 7
-var numero3 = 9
-var promedio = (numero1 + numero2 + numero3) / 3
-console.log("El promedio es: " + promedio)
+let numero1 = 5
+let numero2 = 7
+let numero3 = 9
+let promedio1 = (numero1 + numero2 + numero3) / 3
+console.log("El primer promedio es: " + promedio1)
 
-var numero1 = -2
-var numero2 = -3
-var numero3 = -8
-var promedio = (numero1 + numero2 + numero3) / 3
-console.log("El promedio es: " + promedio)
+numero1 = -2
+numero2 = -3
+numero3 = -8
+promedio2 = (numero1 + numero2 + numero3) / 3
+console.log("El segundo promedio es: " + promedio2)
 
-var numero1 = -1.2
-var numero2 = 5.3
-var numero3 = -3.3
-var promedio = (numero1 + numero2 + numero3) / 3
-console.log("El promedio es: " + promedio)
+numero1 = -1.2
+numero2 = 5.3
+numero3 = -3.3
+promedio3 = (numero1 + numero2 + numero3) / 3
+console.log("El tercer promedio es: " + promedio3)
